050dfddf2e2980dfe96ad1437dcd5e14208cbb5a
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2023-04-14 14:05:32 +0300

    Add Report Portal config

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -1,11 +1,35 @@
 import {defineConfig} from 'cypress';
+import * as registerReportPortalPlugin from '@reportportal/agent-js-cypress/lib/plugin';
 
 export default defineConfig({
+    // Cypress common config
     chromeWebSecurity: false,
     viewportWidth: 1920,
     viewportHeight: 1080,
     e2e: {
         baseUrl: 'https://www.example.com',
         specPattern: 'cypress/e2e/**/*.cy.ts',
-    }
+        setupNodeEvents(on, config) {
+            return registerReportPortalPlugin(on, config);
+        },
+    },
+
+    // Report Portal config
+    reporter: '@reportportal/agent-js-cypress',
+    reporterOptions: {
+        endpoint: 'http://localhost:8080/api/v1',
+        token: '00000000-0000-0000-0000-000000000000',
+        launch: 'Cypress',
+        project: 'default_personal',
+        description: 'Smoke test',
+        attributes: [
+            {
+                key: 'key',
+                value: 'value',
+            },
+            {
+                value: 'tag',
+            },
+        ],
+    },
 });
